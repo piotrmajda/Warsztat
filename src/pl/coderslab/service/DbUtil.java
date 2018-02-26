7b867e05f150fdf7e72f7ad6144485516fe51074
piotrmajda <piotrmajda@gmail.com>
2018-02-26 16:33:03 +0100

    add db connection

---
+++ src/pl/coderslab/service/DbUtil.java
@@ -0,0 +1,25 @@
+package pl.coderslab.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+
+public class DbUtil {
+    private static DataSource ds;
+    public static Connection getConn() throws SQLException {
+        return getInstance().getConnection();
+    }
+    private static DataSource getInstance() {
+        if(ds == null) {
+            try {
+                Context ctx = new InitialContext();
+                ds = (DataSource)ctx.lookup("java:comp/env/jdbc/warsztat");
+            } catch (NamingException e) {
+                e.printStackTrace();}}
+        return ds;}
+}
