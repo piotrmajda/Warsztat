8d928e8705523d642c49b91eff95c1498ad80020
piotrmajda <piotrmajda@gmail.com>
2018-02-27 12:35:43 +0100

    add class employees & orders

---
+++ src/pl/coderslab/controller/employees.java
@@ -0,0 +1,80 @@
+package pl.coderslab.controller;
+
+public class employees {
+	private int id;
+	private String name;
+	private String surname;
+	private String address;
+	private int phone;
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public double getEmployee_hours() {
+		return employee_hours;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public void setEmployee_hours(double employee_hours) {
+		this.employee_hours = employee_hours;
+	}
+
+	private String note;
+	private double employee_hours;
+	
+	public employees(int id, String name, String surname, String address, int phone, String note,
+			double employee_hours) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.address = address;
+		this.phone = phone;
+		this.note = note;
+		this.employee_hours = employee_hours;
+	}
+	
+}
---
+++ src/pl/coderslab/controller/orders.java
@@ -0,0 +1,127 @@
+package pl.coderslab.controller;
+
+public class orders {
+	private String id;
+	private String orderDate;
+	private String startDate;
+	private int employeeId;
+	private String problemDesc;
+	private String repairDesc;
+	private String vehicleId;
+	private double generalCost;
+	private double partsCost;
+	private double employeeHours;
+	private double employeeHourlyRate;
+	private int clientId;
+	
+	public String getId() {
+		return id;
+	}
+
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public String getProblemDesc() {
+		return problemDesc;
+	}
+
+	public String getRepairDesc() {
+		return repairDesc;
+	}
+
+	public String getVehicleId() {
+		return vehicleId;
+	}
+
+	public double getGeneralCost() {
+		return generalCost;
+	}
+
+	public double getPartsCost() {
+		return partsCost;
+	}
+
+	public double getEmployeeHours() {
+		return employeeHours;
+	}
+
+	public double getEmployeeHourlyRate() {
+		return employeeHourlyRate;
+	}
+
+	public int getClientId() {
+		return clientId;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public void setProblemDesc(String problemDesc) {
+		this.problemDesc = problemDesc;
+	}
+
+	public void setRepairDesc(String repairDesc) {
+		this.repairDesc = repairDesc;
+	}
+
+	public void setVehicleId(String vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+
+	public void setGeneralCost(double generalCost) {
+		this.generalCost = generalCost;
+	}
+
+	public void setPartsCost(double partsCost) {
+		this.partsCost = partsCost;
+	}
+
+	public void setEmployeeHours(double employeeHours) {
+		this.employeeHours = employeeHours;
+	}
+
+	public void setEmployeeHourlyRate(double employeeHourlyRate) {
+		this.employeeHourlyRate = employeeHourlyRate;
+	}
+
+	public orders(String id, String orderDate, String startDate, int employeeId, String problemDesc, String repairDesc,
+			String vehicleId, double generalCost, double partsCost, double employeeHours, double employeeHourlyRate,
+			int clientId) {
+		super();
+		this.id = id;
+		this.orderDate = orderDate;
+		this.startDate = startDate;
+		this.employeeId = employeeId;
+		this.problemDesc = problemDesc;
+		this.repairDesc = repairDesc;
+		this.vehicleId = vehicleId;
+		this.generalCost = generalCost;
+		this.partsCost = partsCost;
+		this.employeeHours = employeeHours;
+		this.employeeHourlyRate = employeeHourlyRate;
+		this.clientId = clientId;
+	}
+
+}
